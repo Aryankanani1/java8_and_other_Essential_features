d5f80631c363bbd205c737793838d1d7be70967a
Aryan Kanani <aryankanani57@gmail.com>
2026-08-18 07:47:31 -0400

    auto-sync: 2026-08-18 07:47:31

--- src/main/java/org/example/java8/streams/Main.java
+++ src/main/java/org/example/java8/streams/Main.java
@@ -36,8 +36,7 @@ public class Main {
             }
         }
         System.out.println(sumSquare);
+
+        int sumSquare_stream = list.stream().filter()
     }
-
-    int sumSquare_stream = list.
-
 }
