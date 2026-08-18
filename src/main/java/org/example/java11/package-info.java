584be8be4e5be8eca81a7e5d1e9d923ea54d4f99
Aryan Kanani <aryankanani57@gmail.com>
2026-08-18 07:25:53 -0400

    auto-sync: 2026-08-18 07:25:53

--- src/main/java/org/example/java11/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Examples of features introduced in <strong>Java 11</strong>.
- *
- * <p>HTTP Client API, new String methods, Files.readString/writeString.</p>
- */
-package org.example.java11;
