c79c5a11b7362d620318ff363d4de82416c060b8
Aryan Kanani <aryankanani57@gmail.com>
2026-08-18 07:25:29 -0400

    auto-sync: 2026-08-18 07:25:29

--- src/main/java/org/example/java17/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Examples of features introduced in <strong>Java 17</strong>.
- *
- * <p>Sealed classes (standard), pattern matching for switch (preview).</p>
- */
-package org.example.java17;
