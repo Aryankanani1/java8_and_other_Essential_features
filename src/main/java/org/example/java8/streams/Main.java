206a06e20cc272d1581d5e80416aac64e6cb4104
Aryan Kanani <aryankanani57@gmail.com>
2026-08-18 07:32:35 -0400

    auto-sync: 2026-08-18 07:32:35

--- src/main/java/org/example/java8/streams/Main.java
+++ src/main/java/org/example/java8/streams/Main.java
@@ -5,11 +5,13 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        // before stream
+        //primitive type array
         int [] array = {1,2,3,4,5,6};
         IntStream intStream = Arrays.stream(array);
         intStream.forEach(System.out::println);
 
-        //
+
+        //object type array
+        Integer[] integers = Arr{}
     }
 }
