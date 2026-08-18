30d779826cd17adf9fd56e8613b0e341c4342d1f
Aryan Kanani <aryankanani57@gmail.com>
2026-08-18 07:49:35 -0400

    auto-sync: 2026-08-18 07:49:35

--- src/main/java/org/example/java8/streams/Example1.java
+++ src/main/java/org/example/java8/streams/Example1.java
@@ -1,2 +1,29 @@
-package org.example.java8.streams;public class Example1 {
+package org.example.java8.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example1 {
+    public static void main(String[] args) {
+        // without stream
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        int sumSquare = 0;
+        for(Integer integer : list){
+            if(integer % 2 == 0){
+                int square = integer * integer;
+                sumSquare += square;
+            }
+        }
+        System.out.println(sumSquare);
+
+        int sumSquare_stream = list.stream().filter(number -> number %2 ==0).mapToInt(n -> n* n).sum();
+        System.out.println(sumSquare_stream);
+    }
 }
--- src/main/java/org/example/java8/streams/Main.java
+++ src/main/java/org/example/java8/streams/Main.java
@@ -19,25 +19,5 @@ public class Main {
         Stream<Integer> integerStream = Stream.of(integers);
         integerStream.forEach(System.out::println);
 
-        // without stream
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-
-        int sumSquare = 0;
-        for(Integer integer : list){
-            if(integer % 2 == 0){
-                int square = integer * integer;
-                sumSquare += square;
-            }
-        }
-        System.out.println(sumSquare);
-
-        int sumSquare_stream = list.stream().filter(number -> number %2 ==0).mapToInt(n -> n* n).sum();
-        System.out.println(sumSquare_stream);
     }
 }
