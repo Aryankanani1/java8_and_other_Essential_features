c9fa1818be678c6c7eb3448b5272454df64713d8
Aryan Kanani <aryankanani57@gmail.com>
2026-08-18 07:24:57 -0400

    auto-sync: 2026-08-18 07:24:57

--- src/main/java/org/example/java25/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Examples of features introduced in <strong>Java 25</strong>.
- *
- * <p>Compact source files and instance main methods, scoped values (standard).</p>
- */
-package org.example.java25;
