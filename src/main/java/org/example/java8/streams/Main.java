25dcb9a09ddb9e3bf1f78a52e55882c9a22e0a41
Aryan Kanani <aryankanani57@gmail.com>
2026-08-18 07:30:51 -0400

    auto-sync: 2026-08-18 07:30:51

--- src/main/java/org/example/java8/streams/Main.java
+++ src/main/java/org/example/java8/streams/Main.java
@@ -1,9 +1,13 @@
 package org.example.java8.streams;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         int [] array = {1,2,3,4,5,6};
-        
+        IntStream intStream = Arrays.stream(array);
+        System.out.println("int stream: " + intStream);
 
     }
 }
