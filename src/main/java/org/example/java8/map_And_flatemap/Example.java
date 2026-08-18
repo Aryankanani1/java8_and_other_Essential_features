77299ae0ad7817fdcb0e33ee46b354281c33ed45
Aryan Kanani <aryankanani57@gmail.com>
2026-08-18 07:53:50 -0400

    auto-sync: 2026-08-18 07:53:50

--- src/main/java/org/example/java8/map_And_flatemap/Example.java
+++ src/main/java/org/example/java8/map_And_flatemap/Example.java
@@ -1,2 +1,7 @@
-package org.example.java8.map_And_flatemap;public class Example {
+package org.example.java8.map_And_flatemap;
+
+public class Example {
+    public static void main(String[] args) {
+
+    }
 }
