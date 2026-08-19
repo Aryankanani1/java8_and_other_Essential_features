e5c62ba2046bae525d88545c2227cf93f01e843a
Aryan Kanani <aryankanani57@gmail.com>
2026-08-19 08:05:12 -0400

    auto-sync: 2026-08-19 08:05:12

--- src/main/java/org/example/java8/streams/Main.java
+++ src/main/java/org/example/java8/streams/Main.java
@@ -1,9 +1,8 @@
 package org.example.java8.streams;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.
+        IntStream;
 import java.util.stream.Stream;
 
 public class Main {
